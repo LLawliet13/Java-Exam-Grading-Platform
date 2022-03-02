2ff73d9a88ff0842334f799284c883e4581f81c2
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-03-02 21:38:16 +0700

    Add project files.

---
+++ ChamThiWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ChamThiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChamThiWeb.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ChamThiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChamThiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
