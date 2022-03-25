3fe47948f73dd1cd7e868033de4950dc600f3e7c
Tran Hieu <hieutnhe153199@fpt.edu.vn>
2022-03-25 11:46:50 +0700

    fixBug

--- ChamThiDotnet5/Controllers/TeacherController.cs
+++ ChamThiDotnet5/Controllers/TeacherController.cs
@@ -42,13 +42,22 @@ namespace ChamThiDotnet5.Controllers
 
             ViewBag.ResultClass_Exams = _exam_StudentService.FindResultExamOfTeacher(teacherId);
             
+            
 
             int classid = 0;
             if (ID != null) classid = int.Parse(ID);
+            else classid = int.Parse(idClass);
             Class1(classid);
-
             var model = new FilesViewModel();
             model = load.Index();
+          
+            string addtest = HttpContext.Session.GetString("addtest");
+            if (addtest!=null) if(addtest.Equals("true"))
+            {
+                ViewData["AddExam"] = className + ": Add exam success! View detail in Exam Pending.";
+                    HttpContext.Session.SetString("addtest", "false");
+                    return View("Index",model);
+            }
             ViewData["StudentNum"] = StudentNum;
             ViewBag.StudentList = students;
             ViewBag.Exam = exams;
@@ -81,7 +90,7 @@ namespace ChamThiDotnet5.Controllers
         List<Class> classes;
         int classID =0;
         string className;
-
+        string idClass = "0";
         public void Class1(int id)
         {
             int classid = Convert.ToInt32(id);
@@ -108,92 +117,80 @@ namespace ChamThiDotnet5.Controllers
             if (accid == null) accid = "1";
             var teacher = teacherDAO.GetATercherByAccId(Int32.Parse(accid));
             classes = _classService.GetClassesByTeacherId(teacher.Id);
-          
- 
         }
 
 
 
         // Select class
-        public IActionResult Class(int id)
+        //public IActionResult Class(int id)
+        //{
+        //    int classid = Convert.ToInt32(id);
+        //    if (classid != 0)
+        //    { 
+        //        classID = classid;
+        //        className = classDAO.ReadAClass(classid).Classname;
+        //        var student = studentDAO.ReadAllStudent().ToList();
+        //        int i=0;
+        //        while(i<student.Count)
+        //        {
+        //            if (student[i].ClassId != classid)
+        //            {
+        //                student.Remove(student[i]);
+        //            }
+        //            else i++;
+        //        }
+        //        StudentNum = student.Count; 
+        //        students = student;
+        //        exams = examDAO.ReadAllExam().ToList();
+        //    }
+
+        //    var accid = HttpContext.Session.GetString("accountid");
+        //    if (accid == null) accid = "1";
+        //    var teacher = teacherDAO.GetATercherByAccId(Int32.Parse(accid));
+        //    classes = _classService.GetClassesByTeacherId(teacher.Id);
+        //    ViewData["StudentNum"] = StudentNum;
+        //    ViewBag.StudentList = students;
+        //    ViewBag.Exam = exams;
+        //    ViewData["ClassId"] = classID;
+        //    ViewData["ClassName"] = className;
+        //    ViewBag.ClassList = classes;
+        //    return View();
+        //}
+        
+        // Add exam for all student in selected class
+        [HttpPost]
+        public IActionResult AddTest(string classid, string exam, string start, string end)
         {
-            int classid = Convert.ToInt32(id);
-            if (classid != 0)
-            { 
-                classID = classid;
-                className = classDAO.ReadAClass(classid).Classname;
+            int Classid = Convert.ToInt32(classid);
+            if (Classid != 0)
+            {
+                className = classDAO.ReadAClass(Classid).Classname;
                 var student = studentDAO.ReadAllStudent().ToList();
-                int i=0;
-                while(i<student.Count)
+                int i = 0;
+                while (i < student.Count)
                 {
-                    if (student[i].ClassId != classid)
+                    if (student[i].ClassId != Classid)
                     {
                         student.Remove(student[i]);
                     }
                     else i++;
                 }
-                StudentNum = student.Count; 
-                students = student;
-                exams = examDAO.ReadAllExam().ToList();
+                if (start != null)
+                {
+                    foreach (var s in student)
+                    {
+                        var exSt = new Exam_Student();
+                        exSt.StudentId = s.Id;
+                        exSt.ExamId = examDAO.ReadAExam(int.Parse(exam)).Id;
+                        exSt.Start = DateTime.Parse(start);
+                        exSt.End = DateTime.Parse(end);
+                        exam_studentDAO.AddNewExam_Student(exSt);
+                    }
+                }
+                HttpContext.Session.SetString("addtest", "true");
             }
-
-            var accid = HttpContext.Session.GetString("accountid");
-            if (accid == null) accid = "1";
-            var teacher = teacherDAO.GetATercherByAccId(Int32.Parse(accid));
-            classes = _classService.GetClassesByTeacherId(teacher.Id);
-            ViewData["StudentNum"] = StudentNum;
-            ViewBag.StudentList = students;
-            ViewBag.Exam = exams;
-            ViewData["ClassId"] = classID;
-            ViewData["ClassName"] = className;
-            ViewBag.ClassList = classes;
-            return View();
-        }
-        
-        // Add exam for all student in selected class
-        [HttpPost]
-        public IActionResult Class(string id, string exam, string start, string end)
-        {
-            //int classid = Convert.ToInt32(id);
-            //if (classid != 0)
-            //{
-            //    ViewData["ClassId"] = classid;
-            //    ViewData["ClassName"] = classDAO.ReadAClass(classid).Classname;
-            //    var student = studentDAO.ReadAllStudent().ToList();
-            //    int i = 0;
-            //    while (i < student.Count)
-            //    {
-            //        if (student[i].ClassId != classid)
-            //        {
-            //            student.Remove(student[i]);
-            //        }
-            //        else i++;
-            //    }
-            //    //ViewData["StudentNum"] = student.Count;
-            //    ViewBag.StudentList = student;
-            //    if (start != null) {
-            //        foreach(var s in student)
-            //        {
-            //            var exSt = new Exam_Student();
-            //            exSt.StudentId = s.Id;
-            //            exSt.ExamId = examDAO.ReadAExam(int.Parse(exam)).Id;
-            //            exSt.Start = DateTime.Parse(start);
-            //            exSt.End = DateTime.Parse(end);
-            //            //return View();
-            //            exam_studentDAO.AddNewExam_Student(exSt);
-                        
-            //        }
-            //    }
-            //    ViewData["AddExam"] = ViewData["ClassName"] + ": Add exam success! View detail in Exam Pending.";
-            //    ViewBag.Exam = examDAO.ReadAllExam().ToList();
-            //}
-
-            //var accid = HttpContext.Session.GetString("accountid");
-            //var teacher = teacherDAO.GetATercherByAccId(Int32.Parse(accid));
-            //List<Class> classes = _classService.GetClassesByTeacherId(teacher.Id);
-            //ViewBag.ClassList = classes;
-
-            return View("~/Teacher/ExamPending.cshtml");
+            idClass = classid;
+            return RedirectToAction("Index");
         }
 
 
@@ -229,24 +226,5 @@ namespace ChamThiDotnet5.Controllers
 
             return View("PendingStudentList");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
