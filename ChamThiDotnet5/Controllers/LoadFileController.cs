4dc610f65ba1ce67d7d24aebe8f6ec28a319ac14
Phan Tuan Viet <vietpthe150767@fpt.edu.vn>
2022-03-25 09:47:40 +0700

    123

---
+++ ChamThiDotnet5/Controllers/LoadFileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChamThiDotnet5.Controllers
+{
+    public class LoadFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
