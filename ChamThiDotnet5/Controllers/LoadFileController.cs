2e72156c3e3c6d0be3ac7256a23e8cc4d6b0f2a2
VietPhan565 <86579331+VietPhan565@users.noreply.github.com>
2022-03-25 17:29:03 +0700

    Cac

--- ChamThiDotnet5/Controllers/LoadFileController.cs
+++ ChamThiDotnet5/Controllers/LoadFileController.cs
@@ -1,159 +1,163 @@
-﻿using ChamThiDotnet5.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Threading.Tasks;
+﻿//using ChamThiDotnet5.Models;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Threading.Tasks;
 
-namespace ChamThiDotnet5.Controllers
-{
-    public class LoadfileController : Controller
-    { 
-        public LoadfileController()
-        {
-        }
+//namespace ChamThiDotnet5.Controllers
+//{
+//    public class LoadfileController : Controller
+//    {
+//        public LoadfileController()
+//        {
+//        }
 
-        public string getDefaultFilePath()
-        {
-            string dirPath = "C:/PRNChamThi/DeThi";
+//        public string getDefaultFilePath()
+//        {
+//            string dirPath = "C:/PRNChamThi/DeThi";
 
-            bool exist = Directory.Exists(dirPath);
+//            bool exist = Directory.Exists(dirPath);
 
-            // Nếu không tồn tại, tạo thư mục này.
-            if (!exist)
-            {
-                // Tạo thư mục.
-                Directory.CreateDirectory(dirPath);
-            }
+//            Nếu không tồn tại, tạo thư mục này.
+//            if (!exist)
+//            {
+//                Tạo thư mục.
+//               Directory.CreateDirectory(dirPath);
+//            }
 
-            return dirPath;
-        }
+//            return dirPath;
+//        }
 
-        public FilesViewModel Index()
-        {
-            // Get files from the server
-            var model = new FilesViewModel();
-            //foreach (var item in Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "upload")))
-            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
-            {
-                model.Files.Add(
-                    new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
-            }
-            return model;
-        }
+//        public FilesViewModel Index()
+//        {
+//            Get files from the server
 
-        [HttpPost]
-        public IActionResult Index(IFormFile[] files)
-        {
-            // Iterate each files
-            foreach (var file in files)
-            {
-                // Get the file name from the browser
-                var fileName = System.IO.Path.GetFileName(file.FileName);
+//           var model = new FilesViewModel();
+//            foreach (var item in Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "upload")))
+//                foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
+//                {
+//                    model.Files.Add(
+//                        new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
+//                }
+//            return model;
+//        }
 
-                // Get file path to be uploaded
-                var filePath = Path.Combine(getDefaultFilePath(), fileName);
+//        [HttpPost]
+//        public IActionResult Index(IFormFile[] files)
+//        {
+//            Iterate each files
+//            foreach (var file in files)
+//            {
+//                Get the file name from the browser
 
-                // Check If file with same name exists and delete it
-                if (System.IO.File.Exists(filePath))
-                {
-                    System.IO.File.Delete(filePath);
-                }
+//               var fileName = System.IO.Path.GetFileName(file.FileName);
 
-                // Create a new local file and copy contents of uploaded file
-                using (var localFile = System.IO.File.OpenWrite(filePath))
-                using (var uploadedFile = file.OpenReadStream())
-                {
-                    uploadedFile.CopyTo(localFile);
-                }
-            }
-            ViewBag.Message = "Files are successfully uploaded";
+//                Get file path to be uploaded
+//                var filePath = Path.Combine(getDefaultFilePath(), fileName);
 
-            // Get files from the server
-            var model = new FilesViewModel();
-            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
-            {
-                model.Files.Add(
-                    new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
-            }
-            return View("../Teacher/ExamBank", model);
-        }
+//                Check If file with same name exists and delete it
+//                if (System.IO.File.Exists(filePath))
+//                {
+//                    System.IO.File.Delete(filePath);
+//                }
 
-        public async Task<IActionResult> Download(string filename)
-        {
-            if (filename == null)
-                return Content("filename is not availble");
+//                Create a new local file and copy contents of uploaded file
+//                using (var localFile = System.IO.File.OpenWrite(filePath))
+//                using (var uploadedFile = file.OpenReadStream())
+//                {
+//                    uploadedFile.CopyTo(localFile);
+//                }
+//            }
+//            ViewBag.Message = "Files are successfully uploaded";
 
-            var path = Path.Combine(getDefaultFilePath(), filename);
+//            Get files from the server
 
-            var memory = new MemoryStream();
-            using (var stream = new FileStream(path, FileMode.Open))
-            {
-                await stream.CopyToAsync(memory);
-            }
-            memory.Position = 0;
-            return File(memory, GetContentType(path), Path.GetFileName(path));
-        }
+//           var model = new FilesViewModel();
+//            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
+//            {
+//                model.Files.Add(
+//                    new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
+//            }
+//            return View("../Teacher/ExamBank", model);
+//        }
 
-        private string GetContentType(string path)
-        {
-            var types = GetMimeTypes();
-            var ext = Path.GetExtension(path).ToLowerInvariant();
-            return types[ext];
-        }
 
-        private Dictionary<string, string> GetMimeTypes()
-        {
-            return new Dictionary<string, string>
-            {
-                {".txt", "text/plain"},
-                {".pdf", "application/pdf"},
-                {".doc", "application/vnd.ms-word"},
-                {".docx", "application/vnd.ms-word"},
-                {".xls", "application/vnd.ms-excel"},
-                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
-                {".png", "image/png"},
-                {".jpg", "image/jpeg"},
-                {".jpeg", "image/jpeg"},
-                {".gif", "image/gif"},
-                {".csv", "text/csv"}
-            };
-        }
+//        public async Task<IActionResult> Download(string filename)
+//        {
+//            if (filename == null)
+//                return Content("filename is not availble");
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+//            var path = Path.Combine(getDefaultFilePath(), filename);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+//            var memory = new MemoryStream();
+//            using (var stream = new FileStream(path, FileMode.Open))
+//            {
+//                await stream.CopyToAsync(memory);
+//            }
+//            memory.Position = 0;
+//            return File(memory, GetContentType(path), Path.GetFileName(path));
+//        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteFile(string filename)
-        {
-            if (filename == null)
-                return Content("filename is not availble");
+//        private string GetContentType(string path)
+//        {
+//            var types = GetMimeTypes();
+//            var ext = Path.GetExtension(path).ToLowerInvariant();
+//            return types[ext];
+//        }
 
-            var fullPath = Path.Combine(getDefaultFilePath(), filename);
+//        private Dictionary<string, string> GetMimeTypes()
+//        {
+//            return new Dictionary<string, string>
+//            {
+//                {".txt", "text/plain"},
+//                {".pdf", "application/pdf"},
+//                {".doc", "application/vnd.ms-word"},
+//                {".docx", "application/vnd.ms-word"},
+//                {".xls", "application/vnd.ms-excel"},
+//                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+//                {".png", "image/png"},
+//                {".jpg", "image/jpeg"},
+//                {".jpeg", "image/jpeg"},
+//                {".gif", "image/gif"},
+//                {".csv", "text/csv"}
+//            };
+//        }
 
-            if (System.IO.File.Exists(fullPath))
-            {
-                System.IO.File.Delete(fullPath);
-                TempData["deleted"] = "Files are successfully Deleted";
-            }
-            var model = new FilesViewModel();
-            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
-            {
-                model.Files.Add(
-                    new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
-            }
-            return RedirectToAction("Index");
-        }
-    }
-}
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult DeleteFile(string filename)
+//        {
+//            if (filename == null)
+//                return Content("filename is not availble");
+
+//            var fullPath = Path.Combine(getDefaultFilePath(), filename);
+
+//            if (System.IO.File.Exists(fullPath))
+//            {
+//                System.IO.File.Delete(fullPath);
+//                TempData["deleted"] = "Files are successfully Deleted";
+//            }
+//            var model = new FilesViewModel();
+//            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath())))
+//            {
+//                model.Files.Add(
+//                    new FileDetails { Name = System.IO.Path.GetFileName(item), Path = item });
+//            }
+//            return RedirectToAction("Index");
+//        }
+//    }
+//}
--- ChamThiDotnet5/Controllers/StudentController.cs
+++ ChamThiDotnet5/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using ChamThiDotnet5.DAO;
+﻿using Aspose.Zip.Rar;
+using ChamThiDotnet5.DAO;
 using ChamThiDotnet5.Models;
 using ChamThiWeb5.Data;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,33 @@ namespace ChamThiDotnet5.Controllers
         private StudentDAO daoStudent = new StudentDAO();
         private Exam_StudentDAO daoExamStudent = new Exam_StudentDAO();
         private ExamDAO daoExam = new ExamDAO();
+
+        public string getDefaultFilePath(Student s, Exam_Student exam_Student)
+        {
+            string dirPath = $@"C:\PRNChamThi\{s.ClassId}_{exam_Student.ExamId}\BaiLamHocSinh_{exam_Student.StudentId}";
+
+            bool exist = Directory.Exists(dirPath);
+
+            // Nếu không tồn tại, tạo thư mục này.
+            if (!exist)
+            {
+                // Tạo thư mục.
+                Directory.CreateDirectory(dirPath);
+            }
+
+            return dirPath;
+        }
+
+        public void UnzipFile(string filePath, string directory)
+        {
+            if (filePath.Contains(".rar") || filePath.Contains(".zip"))
+            {
+                using (RarArchive archive = new RarArchive(filePath))
+                {
+                    archive.ExtractToDirectory(directory);
+                }
+            }
+        }
 
         public IActionResult StudentExam()
         {
@@ -80,20 +108,57 @@ namespace ChamThiDotnet5.Controllers
         [HttpPost]
         public IActionResult StudentSubmit(int id, IFormFile[] files)
         {
-            // Iterate each files
+            string student_id = HttpContext.Session.GetString("accountid");
+            
+            Student s = new Student();
+            List<Student> listS = daoStudent.ReadAllStudent();
+            foreach(Student student in listS)
+            {
+                if (student.Id == Convert.ToInt32(student_id))
+                {
+                    s = student;
+                }
+            }
+
+            Exam_Student exS = new Exam_Student();
+            List<Exam_Student> listexS = daoExamStudent.ReadAllExam_Student();
+            foreach(Exam_Student exstudent in listexS)
+            {
+                if (exstudent.StudentId == s.Id)
+                {
+                    exS = exstudent;
+                }
+            }
+
             foreach (var file in files)
             {
                 // Get the file name from the browser
-                var fileName = Path.GetFileName(file.FileName);
+                var fileName = System.IO.Path.GetFileName(file.FileName);
 
                 // Get file path to be uploaded
-                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Upload", fileName);
+                var filePath = Path.Combine(getDefaultFilePath(s, exS), fileName);
 
-                // Check If file with same name exists and delete it
-                if (System.IO.File.Exists(filePath))
+                var str = Directory.GetFiles(Path.Combine(getDefaultFilePath(s, exS)));
+                var direct = Directory.GetDirectories(Path.Combine(getDefaultFilePath(s, exS)));
+                if (str.Length > 0)
                 {
-                    System.IO.File.Delete(filePath);
+                    foreach (var item in str)
+                    {
+                        System.IO.File.Delete(item);
+                    }
                 }
+                if (direct.Length > 0)
+                {
+                    foreach(var item in direct)
+                    {
+                        Directory.Delete(item, true);
+                    }
+                }
+                //// Check If file with same name exists and delete it
+                //if (System.IO.File.Exists(filePath))
+                //{
+                //    System.IO.File.Delete(filePath);
+                //}
 
                 // Create a new local file and copy contents of uploaded file
                 using (var localFile = System.IO.File.OpenWrite(filePath))
@@ -105,12 +170,19 @@ namespace ChamThiDotnet5.Controllers
 
             // Get files from the server
             var model = new Exam_Student();
-            foreach (var item in Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Upload")))
+            foreach (var item in Directory.GetFiles(Path.Combine(getDefaultFilePath(s,exS))))
             {
-                model = new Exam_Student { SubmittedFolder = item.ToString() };
+                model = new Exam_Student { SubmittedFolder = item };
             }
 
+            Exam_Student exam_Student = daoExamStudent.ReadAExam_Student(id);
+            exam_Student.SubmittedFolder = model.SubmittedFolder;
+            daoExamStudent.UpdateExam_Student(id, exam_Student);
 
+            string directory = $@"C:\PRNChamThi\{s.ClassId}_{exS.ExamId}\BaiLamHocSinh_{exS.StudentId}";
+            UnzipFile(exS.SubmittedFolder, directory);
+
+            ViewBag.Exam_Student = exam_Student;
             return View();
         }
     }
